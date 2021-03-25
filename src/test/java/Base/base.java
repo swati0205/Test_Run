ce0280964626e6510b7f39a1deed355b054a1fde
swati0205 <swati0205.in@gmail.com>
2021-03-24 22:07:09 -0400

    POM change

--- src/test/java/Base/base.java
+++ src/test/java/Base/base.java
@@ -1,13 +1,23 @@
 package Base;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class base {
+
+    public static WebDriver driver;
+
 
     @Test
     public void initiliaze()
     {
         System.out.println("Print");
+        System.setProperty("webdriver.chrome.driver", "/Users/swatisingh/Documents/DevTools/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://www.amazon.ca");
+        driver.manage().window().maximize();
+
     }
 }
 
